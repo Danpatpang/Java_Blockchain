a0620cb47d77285e8481f96fad1266622137d5ac
Danpatpang <puri8467@gmail.com>
2018-06-15 17:31:54 +0900

    Implement basic blockchain
    블록 생성, 채굴, 유효성 검증, gson(json)으로 변환하는 기능 구현

--- src/block/Block.java
+++ src/block/Block.java
@@ -1,26 +1,63 @@
 package block;
 import java.util.Date;
 /**
- * Making the block
+ * Implement the Block.
+ * Each Block contains hash, previousHash, data, timeStamp, nonce.
+ * Hash is determined by sha-256 algorithm.
  * 
  * @author Danpatpang
+ * @date   2018. 06. 15.
  */
 public class Block {
 	public String hash;
 	public String previousHash;
 	private String data;
 	private long timeStamp;
+	private int nonce;
 	
+	/**
+	 * 데이터와 이전 해시 값을 입력받은 후 블록을 초기화.
+	 * 현재 해시 값의 경우는 calculateHash() 메서드를 통해서 정의.
+	 * 
+	 * @param data
+	 * @param previousHash
+	 */
 	public Block(String data, String previousHash) {
 		this.previousHash = previousHash;
 		this.data = data;
 		this.timeStamp = new Date().getTime();
 		this.hash = calculateHash(); 
 	}
-	
+
+	/**
+	 * sha-256 알고리즘을 적용하여 새로운 해시 값을 만들어내는 메서드.
+	 * 블록의 모든 정보를 해시화(암호화).
+	 * nonce를 이용하여 다른 값으로 계속 바꿀 수 있음.
+	 * 
+	 * @return calculatHash
+	 */
 	public String calculateHash() {
-		String calculateHash = StringUtil.applySha256(previousHash+Long.toString(timeStamp)+data);
+		String calculateHash = StringUtil.applySha256(previousHash 
+				+ Long.toString(timeStamp) 
+				+ Integer.toString(nonce) 
+				+ data);
 		return calculateHash;
 	}
 	
+	/**
+	 * 블록을 채굴하는 메서드.
+	 * 채굴의 난이도를 입력받아 문제를 생성.
+	 * 해당 문제가 해결될 때까지 hash 값 재계산.
+	 * 
+	 * @param difficulty
+	 */
+	public void mineBlock(int difficulty) {
+		String target = new String(new char[difficulty]).replace('\0', '0');
+		while(!hash.substring(0, difficulty).equals(target)) {
+			nonce++;
+			hash = calculateHash();
+		}
+		System.out.println(nonce);
+		System.out.println("Block Mined! : " + hash);
+	}
 }
--- src/block/StringUtil.java
+++ src/block/StringUtil.java
@@ -3,23 +3,32 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 /**
- * Generate digital signature
+ * Generate digital signature.
+ * Using sha-256 algorithm.
+ * Input string converts 64bit hexadecimal String.
  * 
  * @author Danpatpang
+ * @date   2018.06.14.
  */
 public class StringUtil {
+	/**
+	 * 입력 값에 sha-256 알고리즘을 적용하는 메서드.
+	 * 알고리즘이 적용된 데이터를 각각 16진수화.
+	 * 16진수로 포맷팅 후 64bit를 hexString에 저장.
+	 * hexString을 String 타입으로 반환.
+	 * 
+	 * @param input
+	 * @return hexString.toString()
+	 */
 	public static String applySha256(String input) {
 		try {
-			// 32바이트
 			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-			// 암호화
 			byte[] hash = digest.digest(input.getBytes("UTF-8"));
 			StringBuffer hexString = new StringBuffer();
-			// 16진수화
 			for (int i = 0; i < hash.length; i++) {
-				// 2바이트 & 1바이트 => 2바이트 32*2 = 64
+				/* 포맷팅 (8 bit & n bit => 8 bit) */
 				String hex = Integer.toHexString(0xff & hash[i]);
-				// 01일 경우 1로 인식하므로 (항상 2자리를 만들어 주겠다.)
+				/* 값이 0x10 미만일 경우 2자리로 통일하기 위해 '0'을 추가 */
 				if(hex.length() == 1) {
 					hexString.append('0');
 				}
