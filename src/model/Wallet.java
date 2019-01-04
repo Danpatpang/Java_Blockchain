ff106c7495e9b839a596cac4e935674d40566b2a
danpatpang <puri8467@gmail.com>
2019-01-05 01:51:22 +0900

    Implement Wallet

---
+++ src/model/Wallet.java
@@ -0,0 +1,41 @@
+package model;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+
+public class Wallet {
+    private PrivateKey privateKey;
+    public PublicKey publicKey;
+
+    public Wallet() {
+        generateKeyPair();
+    }
+
+    // https://en.wikipedia.org/wiki/Elliptic-curve_cryptography
+    // https://www.ssl.com/article/comparing-ecdsa-vs-rsa/
+
+    // key pair 만드는데 사용.
+    private void generateKeyPair() {
+        try {
+            Security.addProvider(new BouncyCastleProvider());
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("ECDSA", "BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("secp256k1");
+
+            generator.initialize(ecSpec, random);
+            KeyPair keyPair = generator.generateKeyPair();
+
+            this.privateKey = keyPair.getPrivate();
+            this.publicKey = keyPair.getPublic();
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidAlgorithmParameterException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        }
+    }
+}
